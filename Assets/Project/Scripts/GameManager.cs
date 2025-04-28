e0cde0b53b9b6341b7605ea90f62f5449ba8e8e7
ArthurRodrigomarques <arthurrodrigo05052004@gmail.com>
2025-04-28 07:43:38 -0300

    add
    add

--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -3,8 +3,13 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    // vida do player
     public Slider sliderLife;
     public static GameManager instance;
+
+    // Pause
+    public GameObject painelMenu;
 
     void Awake()
     {
@@ -13,12 +18,35 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        // vida
         sliderLife.maxValue = 100; // A vida inicial máxima
         sliderLife.value = 100;
+
+        // pause
+        painelMenu.SetActive(false);
     }
 
     public void UpdateLifeSlider(int currentLife)
     {
         sliderLife.value = currentLife;
+    }
+
+    void Update()
+    {
+        Pause();
+    }
+
+    void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) & Time.timeScale == 1) 
+        {
+            Time.timeScale = 0;
+            painelMenu.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) & Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            painelMenu.SetActive(false);
+        }
     }
 }
